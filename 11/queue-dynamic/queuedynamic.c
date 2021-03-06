6e91aa5110a542c2e572c5648101b1aabc7793de
ashutosh6120 <ashutosh4064@gmail.com>
2021-03-06 12:34:01 +0530

    added queue program

---
+++ 11/queue-dynamic/queuedynamic.c
@@ -0,0 +1,87 @@
+/*Queue - Linked List implementation*/
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+struct node *front = NULL;
+struct node *rear = NULL;
+
+void display();
+void enqueue(int);
+void dequeue();
+
+int main()
+{
+    int n, ch;
+    while(1)
+    {
+        printf("\n1:insert\n2:delete\n3:Display\n4:Exit");
+        printf("\nEnter Choice: ");
+        scanf("%d", &ch);
+        switch (ch)
+        {
+            case 1:
+                printf("\nEnter number ");
+                scanf("%d", &n);
+                enqueue(n);
+                break;
+            case 2:
+                dequeue();
+                break;
+            case 3:
+                display();
+                break;
+            default: exit(1);
+        }
+    }
+}
+
+void enqueue(int item)
+{
+    struct node *nptr = malloc(sizeof(struct node));
+    nptr->data = item;
+    nptr->next = NULL;
+    if (rear == NULL)
+    {
+        front = nptr;
+        rear = nptr;
+    }
+    else
+    {
+        rear->next = nptr;
+        rear = rear->next;
+    }
+}
+
+void display()
+{
+    struct node *temp;
+    temp = front;
+    printf("Queue is:\n");
+    while (temp != NULL)
+    {
+        printf("%d\t", temp->data);
+        temp = temp->next;
+    }
+}
+
+void dequeue()
+{
+    if (front == NULL)
+    {
+        printf("queue is empty \n");
+    }
+    else
+    {
+        struct node *temp;
+        temp = front;
+        front = front->next;
+        printf("%d deleted\n", temp->data);
+        free(temp);
+    }
+}
